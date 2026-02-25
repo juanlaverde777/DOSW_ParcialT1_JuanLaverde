fc6b8fc6784213dadfd5fde85823f8dfcc630e70
juanlaverde777 <josefolaverde@gmail.com>
2026-02-25 12:32:44 -0500

    Proyecto base funcionando

--- DOSW-ParcialT1/src/main/java/edu/dosw/parcial/App.java
+++ DOSW-ParcialT1/src/main/java/edu/dosw/parcial/App.java
@@ -1,11 +1,8 @@
-﻿package edu.dosw.parcial;
+package edu.dosw.parcial;
 
 public class App {
-    public static void main(String[] args) {
-        System.out.println(\"Hola desde DOSW-ParcialT1\");
-    }
 
-    public static int suma(int a, int b) {
-        return a + b;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
